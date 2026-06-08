1f877dd3b05aa3e4e6f785f84ab982dd769023c3
Lee <149314728+MarkLeejanAlipio@users.noreply.github.com>
2026-06-08 12:11:20 +0800

    feat: Add playGame function

--- index.js
+++ index.js
@@ -23,3 +23,88 @@ function createPlayer(name, marker) {
     const getScore = () => score;
     return { name, marker, getScore, addScore } 
 } 
+
+function playGame(playerOneName = 'Player One', playerTwoName = 'Player Two') {
+    const playerOne = createPlayer(playerOneName, 'X');
+    const playerTwo = createPlayer(playerTwoName, 'O');
+
+    const WINNING_COMBINATIONS = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8], 
+        [0, 3, 6],
+        [1, 4, 7], 
+        [2, 5, 8], 
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+
+    let activePlayer = playerOne;
+
+    let isGameOver = false;
+
+    const getLiveBoard = () => gameBoard.getBoard();
+    
+    const resetLiveBoard = () => {
+        gameBoard.resetBoard();
+        activePlayer = playerOne;
+        isGameOver = false;
+    }
+    
+    function switchPlayer() {
+        activePlayer = activePlayer === playerOne ? playerTwo : playerOne;
+    }
+
+    const liveRound = () => {
+        console.log(`${activePlayer.name}'s turn`);
+        console.log(getLiveBoard());
+    }
+
+    const hasWinner = () => {
+        const board = gameBoard.getBoard();
+        return WINNING_COMBINATIONS.some(([a, b, c]) => {
+            return (
+                board[a] !== '' &&
+                board[a] === board[b] &&
+                board[a] === board[c]
+            );
+        });
+    }
+
+    const isDraw = () => {
+        return gameBoard.getBoard().every(cell => cell !== '');
+    }
+
+
+    const playRound = (index) => {
+        if (isGameOver) return false;
+
+        const playerMove = gameBoard.placeMarker(index, activePlayer.marker)
+        if (!playerMove) return false;
+
+        if (hasWinner()) {
+            activePlayer.addScore();
+            isGameOver = true;
+            return `${activePlayer.name} won!`;
+        }
+
+        if (isDraw()) {
+            isGameOver = true;  
+            return `Draw`;
+        }
+
+        switchPlayer();
+        liveRound();
+
+        return true;
+    }
+
+    liveRound();
+
+    return {
+        getLiveBoard,
+        resetLiveBoard,
+        liveRound,
+        playRound,
+    }
+};
